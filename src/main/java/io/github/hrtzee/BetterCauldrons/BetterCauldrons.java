d1a4a29b0163596068611d7e553b47f0d3dbab10
Hrtzee <Hrtzee@163.com>
2021-09-30 08:12:11 +0800

    delete useless Networking system

--- src/main/java/io/github/hrtzee/BetterCauldrons/BetterCauldrons.java
+++ src/main/java/io/github/hrtzee/BetterCauldrons/BetterCauldrons.java
@@ -2,9 +2,7 @@ package io.github.hrtzee.BetterCauldrons;
 
 
 import io.github.hrtzee.BetterCauldrons.Items.ItemRegistry;
-import io.github.hrtzee.BetterCauldrons.Networking.NetWorking;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Utils.MOD_ID)
@@ -12,9 +10,5 @@ public class BetterCauldrons {
 
     public BetterCauldrons() {
         ItemRegistry.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetUp);
-    }
-    public void onCommonSetUp(FMLCommonSetupEvent event){
-        NetWorking.registerMessage();
     }
 }
