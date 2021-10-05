453860a59908fd04db279061ca8ca311cc8493c6
Hrtzee <Hrtzee@163.com>
2021-10-05 21:22:00 +0800

    add more things

--- src/main/java/io/github/hrtzee/BetterCauldrons/Events/CauldronEvent.java
+++ src/main/java/io/github/hrtzee/BetterCauldrons/Events/CauldronEvent.java
@@ -30,7 +30,6 @@ import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.MutableBoundingBox;
-import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
